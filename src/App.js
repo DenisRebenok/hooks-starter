5ae0effdd9af2f527b33590b68ac24453cbbb2a1
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-16 23:17:57 +0300

    Refactoring A Class Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
   const [name, setName] = useState('');
   return (
     <div className="main-wrapper">
       <h1>Denys Dishes</h1>
+      <Toggle />
       <h3>{name}</h3>
       <input type="text" onChange={e => setName(e.target.value)} value={name} />
     </div>
