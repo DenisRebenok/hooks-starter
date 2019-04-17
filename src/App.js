ce9b7d5caaca4dfd661dd8b164b222f06349faa2
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-04-17 23:57:55 +0300

    useMemo For Expensive Functions

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import React, { useState, useEffect, useRef, createContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  createContext,
+  useMemo
+} from 'react';
 import useTitleInput from './hooks/useTitleInput';
 import Toggle from './Toggle';
-import Counter from './Counter';
 
-export const UserContext = createContext();
+export const UserContext = createContext({ user: true });
 
 const App = () => {
   const [name, setName] = useTitleInput('');
   const ref = useRef();
 
+  const reverseWord = word => {
+    console.log('function called');
+    return word
+      .split('')
+      .reverse()
+      .join('');
+  };
+
+  const title = 'Denys Dishes';
+
+  const titleReversed = useMemo(() => reverseWord(name), [name]);
+
   return (
-    <UserContext.Provider value={{ user: false }}>
-      <div className="main-wrapper" ref={ref}>
-        <h1 onClick={() => ref.current.classList.add('new-fake-class')}>
-          Denys Dishes
-        </h1>
-        <Toggle />
-        <Counter />
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-          }}
-        >
-          <input
-            type="text"
-            onChange={e => setName(e.target.value)}
-            value={name}
-          />
-          <button>Submit</button>
-        </form>
-      </div>
-    </UserContext.Provider>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>
+        {titleReversed}
+      </h1>
+      <Toggle />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
